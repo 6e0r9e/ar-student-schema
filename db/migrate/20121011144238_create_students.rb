188b093ad55d764ff2dfbc45e2f99eb18f178a7d
6e0r9e <geoge@6e0r9e.com>
2013-02-14 17:39:27 -0800

    Create migration

--- db/migrate/20121011144238_create_students.rb
+++ db/migrate/20121011144238_create_students.rb
@@ -4,6 +4,26 @@ require_relative '../config'
 
 class CreateStudents < ActiveRecord::Migration
   def change
-    # HINT: checkout ActiveRecord::Migration.create_table
+    create_table :students do |table|
+      table.string :first_name
+      table.string :last_name
+      table.string :gender
+      table.string :birthday
+      table.string :email
+      table.string :phone
+    end
   end
 end
+
+
+=begin
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name VARCHAR(64) NOT NULL,
+  last_name  VARCHAR(64) NOT NULL,
+  email VARCHAR(128) UNIQUE NOT NULL,
+  created_at DATETIME NOT NULL,
+  updated_at DATETIME NOT NULL
+);
+
+=end
